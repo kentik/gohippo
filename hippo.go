efb8808193bd6fa712a521dbcce75cba9f7ba9b1
Dhammika Pathirana <dhammika@gmail.com>
2018-04-16 15:11:06 -0700

    Add json encoding
    Signed-off-by: Dhammika Pathirana <dhammika@gmail.com>

--- hippo.go
+++ hippo.go
@@ -3,6 +3,7 @@ package hippo
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -13,6 +14,43 @@ type Client struct {
 	UsrAgent string
 	UsrEmail string
 	UsrToken string
+}
+
+type Rule struct {
+	Dir                string   `json:"direction,omitempty"`
+	Ports              []string `json:"ports,omitempty"`
+	Protocols          []uint   `json:"protocols,omitempty"`
+	ASNs               []string `json:"asns,omitempty"`
+	LastHopASNNames    []string `json:"last_hop_asn_names,omitempty"`
+	NextHopASNs        []string `json:"next_hop_asns,omitempty"`
+	NextHopASNNames    []string `json:"next_hop_ans_names,omitempty"`
+	BGPASPaths         []string `json:"bgp_as_paths,omitempty"`
+	BGPCommunities     []string `json:"bgp_communities,omitempty"`
+	TCPFlags           uint16   `json:"tcp_flags,omitempty"`
+	IPAddresses        []string `json:"ip_addresses,omitempty"`
+	MACAddresses       []string `json:"mac_addresses,omitempty"`
+	CountryCodes       []string `json:"country_codes,omitempty"`
+	SiteNames          []string `json:"site_names,omitempty"`
+	DeviceTypes        []string `json:"device_types,omitempty"`
+	InterfaceNames     []string `json:"interface_names,omitempty"`
+	DeviceNames        []string `json:"device_names,omitempty"`
+	NextHopIPAddresses []string `json:"next_hop_ip_addresses,omitempty"`
+}
+
+type Upsert struct {
+	Val   string `json:"value"`
+	Rules []Rule `json:"criteria,omitempty"`
+}
+
+type Delete struct {
+	Val string `json:"value"`
+}
+
+type Req struct {
+	Replace  bool     `json:"replace_all"`
+	Complete bool     `json:"complete"`
+	Upserts  []Upsert `json:"upserts,omitempty"`
+	Deletes  []Delete `json:"deletes,omitempty"`
 }
 
 func NewHippo(agent string, email string, token string) *Client {
@@ -53,4 +91,12 @@ func (c *Client) Do(ctx context.Context, req *http.Request) ([]byte, error) {
 		return nil, err
 	}
 	return buf, nil
+}
+
+func (c *Client) EncodeReq(r *Req) ([]byte, error) {
+	if b, err := json.Marshal(r); err != nil {
+		return nil, err
+	} else {
+		return b, nil
+	}
 }
--- hippo_test.go
+++ hippo_test.go
@@ -75,16 +75,102 @@ func TestHippPost(t *testing.T) {
 
 	email := os.Getenv("API_EMAIL")
 	token := os.Getenv("API_TOKEN")
-    if email == "" || token == "" {
+	if email == "" || token == "" {
 		t.Errorf("email or token not set\n")
-        return
-    }
+		return
+	}
 	h := NewHippo("", email, token)
-	if req, err := h.NewRequest("POST", "https://api.kentik.com/api/v5/tagging/tags", []byte(body)); err != nil {
+	if req, err := h.NewRequest("POST", "https://api.kentik.com/api/v5/batch/tags", []byte(body)); err != nil {
 		t.Errorf("req err %v\n", err)
 	} else {
 		if _, err := h.Do(context.Background(), req); err != nil {
 			t.Errorf("resp err %v\n", err)
+		}
+	}
+}
+
+func TestHippJsonPost(t *testing.T) {
+	req := &Req{
+		Replace:  true,
+		Complete: true,
+		Upserts: []Upsert{
+			{
+				Val: "foo",
+				Rules: []Rule{
+					{
+						Dir:                "dst",
+						Ports:              []string{"4", "9", "1000", "555", "666", "44"},
+						Protocols:          []uint{1, 2, 4, 5},
+						ASNs:               []string{"12345-20000", "500", "10-100"},
+						LastHopASNNames:    []string{"asn1", "asn2"},
+						NextHopASNs:        []string{"100-200", "35628", "60000-70000"},
+						NextHopASNNames:    []string{"asn2", "asn3"},
+						BGPASPaths:         []string{"as path 1", "as path 2"},
+						BGPCommunities:     []string{"community1", "community2"},
+						TCPFlags:           11,
+						IPAddresses:        []string{"1.2.3.4", "3.4.5.6"},
+						MACAddresses:       []string{"01:42:12:ae:92:bf", "03:42:1f:1e:22:bf"},
+						CountryCodes:       []string{"US", "EN"},
+						SiteNames:          []string{"site1", "site2"},
+						DeviceTypes:        []string{"device_type1", "device_type2"},
+						InterfaceNames:     []string{"interface1", "interface2"},
+						DeviceNames:        []string{"device1", "device2"},
+						NextHopIPAddresses: []string{"10.3.4.5", "9.6.7.8"},
+					},
+				},
+			},
+			{
+				Val: "bar",
+				Rules: []Rule{
+					{
+						Dir:                "dst",
+						Ports:              []string{"3", "2", "3-4", "1-5", "11", "9", "1000", "555", "666", "44"},
+						Protocols:          []uint{1, 2, 3, 4},
+						ASNs:               []string{"12345", "23456", "100-200"},
+						LastHopASNNames:    []string{"asn1", "asn2"},
+						NextHopASNs:        []string{"12345", "1-100", "23456"},
+						NextHopASNNames:    []string{"asn2", "asn3"},
+						BGPASPaths:         []string{"as path 1", "as path 2"},
+						BGPCommunities:     []string{"community1", "community2"},
+						TCPFlags:           12,
+						IPAddresses:        []string{"1.2.3.4", "3.4.5.6"},
+						MACAddresses:       []string{"01:42:12:ae:92:bf", "03:42:1f:1e:22:bf"},
+						CountryCodes:       []string{"US", "EN"},
+						SiteNames:          []string{"site1", "site2"},
+						DeviceTypes:        []string{"device_type1", "device_type2"},
+						InterfaceNames:     []string{"interface1", "interface2"},
+						DeviceNames:        []string{"device1", "device2"},
+						NextHopIPAddresses: []string{"2.9.4.5", "5.9.7.8"},
+					},
+				},
+			},
+		},
+		Deletes: []Delete{
+			{Val: "foo0"},
+			{Val: "bar0"},
+		},
+	}
+
+	email := os.Getenv("API_EMAIL")
+	token := os.Getenv("API_TOKEN")
+	if email == "" || token == "" {
+		t.Errorf("email or token not set\n")
+		return
+	}
+	h := NewHippo("", email, token)
+
+	b, err := h.EncodeReq(req)
+	if err != nil {
+		t.Errorf("req encoding err %v\n", err)
+		return
+	}
+
+	if req, err := h.NewRequest("POST", "https://api.kentik.com/api/v5/batch/tags", b); err != nil {
+		t.Errorf("req err %v\n", err)
+	} else {
+		if _, err := h.Do(context.Background(), req); err != nil {
+			t.Errorf("resp err %v\n", err)
+			t.Errorf("req: %v\n", string(b))
 		}
 	}
 }
